d4bef22132b20f78e70926a7f921103ab639d176
Philipp <p.rednaz@googlemail.com>
2019-08-30 18:00:38 +0200

    along_path_gsap_bezier.js

---
+++ animation/along_path_gsap_bezier.js
@@ -0,0 +1,113 @@
+const library2 = {};
+{
+  const timeline_align_position_static_start =
+    (
+      object,
+      duration,
+      target,
+      options,
+      object_absolute_position,
+      target_absolute_position
+    ) => {
+      const interim_result_target = library.transform_outer_target(target[0], target_absolute_position);
+      const timeline = new TimelineMax();
+      object.forEach(object_current => {
+        const interim_result_object = library.transform_outer_object(object_current, object_absolute_position);
+        timeline.fromTo(
+          object_current,
+          duration,
+          {
+            "x": interim_result_object.transformation_animated.e,
+            "y": interim_result_object.transformation_animated.f
+          },
+          Object.assign(
+            library.transform_inner(
+              interim_result_object,
+              interim_result_target
+            ),
+            options
+          ),
+          0
+        );
+      });
+      return timeline;
+    };
+  const coordinate_2d = unstructured => {
+    const x1 = unstructured.filter((element, index) => index > 0 && index%2 === 1);
+    const x2 = unstructured.filter((element, index) => index > 0 && index%2 === 0);
+    return x1.map((value, index) => ({"x": value, "y": x2[index]}));
+  };
+  const bezier = path =>
+    Array.prototype.concat.apply(
+      [],
+      Snap.path.toCubic(path.getAttribute("d")).map(coordinate_2d)
+    );
+  const timeline_along_path = // preserves options argument owing to Object.assign
+    (function_name, object, duration, path, options, object_absolute_position) => {
+      const timeline = new TimelineMax();
+      object.forEach(object_current => {
+        const options_current = Object.assign({}, options);
+        const interim_result_object = library.transform_outer_object(object_current, object_absolute_position);
+        options_current.bezier = {
+          "type": "cubic",
+          "values": bezier(path[0]).map(coordinate =>
+            library.transform_inner(
+              interim_result_object,
+              {
+                "defining_element": path[0],
+                "defining_element_coordinate": coordinate
+              }
+            )
+          )
+        };
+        timeline[function_name](
+          object_current,
+          duration,
+          options_current,
+          0
+        );
+      });
+      return timeline;
+    };
+  // export
+  library2.timeline_align_position_static_start = timeline_align_position_static_start;
+  library2.timeline_along_path = timeline_along_path;
+}
+
+const absolute_position = { // applicable to player and ball
+  "defining_element": object => object.querySelector("circle"),
+  "coordinate": defining_element => ({
+    "x": defining_element.cx.baseVal.value,
+    "y": defining_element.cy.baseVal.value
+  })
+};
+
+music_set("animation/september.wav");
+svg_set("animation/along_path_gsap_bezier.svg");
+
+svg_main.addEventListener("load", () => {
+  timeline.add(library.ball_along_path(
+    svg("#ball1"),
+    5,
+    svg("#move1"),
+    {ease: Power0.easeNone}
+  ));
+  timeline.seek(2);
+  const pass = library.timeline_align_position_static_start(
+  // const pass = library.timeline_align_position("to", // surprisingly gives the third animation the chance to set the worng start position
+    svg("#ball2"),
+    2,
+    svg("#ball1"),
+    {ease: Power0.easeNone, overwrite: "none"}, // both "all" and "none" work fine for overwrite, "auto" surprisingly kills the third animation
+    absolute_position,
+    absolute_position
+  );
+  timeline.seek(0);
+  timeline.add(ball_along_path(
+    svg("#ball2"),
+    5,
+    svg("#move1"),
+    {ease: Power0.easeNone}
+  ), 0); // setting this positive suprsingly causes pass to be overwritten regardless of its overwrite setting
+  timeline.add(pass, 0);
+});
