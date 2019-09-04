dcb2f11b773ca6575d64a307a0975089f2cc1cef
Philipp <p.rednaz@googlemail.com>
2019-09-04 19:55:34 +0200

    cleanup

--- animation/testspielzug.js
+++ animation/testspielzug.js
@@ -7,20 +7,20 @@ svg_main.addEventListener("load", () => {
   clock();
 
   //player1
-  timeline.add(player_move(1, time(3), 1,), time(1,1));
-  timeline.add(player_move(1, time(10), 2,), time(2,1));
+  timeline.add(player_move(1, time(3), 1), time(1,1));
+  timeline.add(player_move(1, time(10), 2), time(2,1));
 
   //player3
-  timeline.add(player_move(3, time(3), 7,), time(1,1));
-  timeline.add(player_move(3, time(13), 8,), time(1,4));
+  timeline.add(player_move(3, time(3), 7), time(1,1));
+  timeline.add(player_move(3, time(13), 8), time(1,4));
 
   //player4
-  timeline.add(player_move(4, time(3), 5,), time(1,1));
-  timeline.add(player_move(4, time(9), 6,), time(2,3));
+  timeline.add(player_move(4, time(3), 5), time(1,1));
+  timeline.add(player_move(4, time(9), 6), time(2,3));
 
   //player5
-  timeline.add(player_move(5, time(4), 3,), time(1,1));
-  timeline.add(player_move(5, time(3), 4,), time(2,3));
+  timeline.add(player_move(5, time(4), 3), time(1,1));
+  timeline.add(player_move(5, time(3), 4), time(2,3));
 
   //ball
   pass(5, time(1,4), time(2,1));
