d383719a42c9eb620dd5cb71a68975fa3d8f65d5
Stefan Krühler <stefankruhler@MacBook-Pro-4.local>
2020-08-23 01:54:51 +0200

    replace testspielzug2.js

--- animation/testspielzug2.js
+++ animation/testspielzug2.js
@@ -14,7 +14,7 @@ const {
   shoot,
   startup_animation
 } = basketball_animate({
-  svg_source: "animation/testspielzug.svg",
+  svg_source: "animation/testspielzug2.svg",
   music_source: "animation/september.wav",
   music_offset: 0.09,
   beats_per_minute: 120,
@@ -30,25 +30,39 @@ svg_main.addEventListener("load", () => {
 
   //player1
   player_move("#player1", "#move1", time(1,1), time(1,4));
-  timeline.add(player_move_tween("#player1", "#move2", time(4,3) - time(2,1)), time(2,1));
+  player_move("#player1", "#move2", time(2,1), time(2,4));
+  player_move("#player1", "#move1", time(3,1), time(3,4));
+  player_move("#player1", "#move2", time(4,1), time(4,4));
+  player_move("#player1", "#move1", time(5,1), time(5,4));
+
+  //player2
+  player_move("#player2", "#move3", time(1,1), time(2,4));
+  player_move("#player2", "#move4", time(3,1), time(3,4));
+  player_move("#player2", "#move5", time(4,1), time(4,4));
+  player_move("#player2", "#move6", time(5,1), time(5,4));
 
   //player3
-  timeline.add(player_move_tween("#player3", "#move7", time_duration(0,3)), time(1,1));
-  timeline.add(player_move_tween("#player3", "#move8", time_duration(0,13)), time(1,4));
+  player_move("#player3", "#move7", time(1,1), time(1,4));
+  player_move("#player3", "#move8", time(2,1), time(2,2));
+  player_move("#player3", "#move9", time(2,3), time(3,2));
+  player_move("#player3", "#move7", time(3,2), time(4,1));
+  player_move("#player3", "#move8", time(4,1), time(5,1));
 
   //player4
-  timeline.add(player_move_tween("#player4", "#move5", time_duration(0,3)), time(1,1));
-  timeline.add(player_move_tween("#player4", "#move6", time_duration(0,9)), time(2,3));
+  player_move("#player4", "#move10", time(1,1), time(5,1));
 
   //player5
-  timeline.add(player_move_tween("#player5", "#move3", time_duration(0,4)), time(1,1));
-  timeline.add(player_move_tween("#player5", "#move4", time_duration(0,3)), time(2,3));
+  player_move("#player5", "#move11", time(1,1), time(5,4));
+
 
   //ball
-  pass("#player1", "#player5", time(1,3), time(1,4));
-  pass("#player5", "#player2", time(2,2), time(2,3));
-  pass("#player2", "#player4", time(3,2), time(3,3));
-  shoot("#player4", time(4,4), time(5, 1));
+  pass("#player1", "#player2", time(1,3), time(1,4));
+  pass("#player2", "#player3", time(2,1), time(2,2));
+  pass("#player3", "#player4", time(2,3), time(2,4));
+  pass("#player4", "#player5", time(3,1), time(3,2));
+  pass("#player5", "#player1", time(3,3), time(3,4));
+  pass("#player1", "#player5", time(4,1), time(4,2));
+  shoot("#player5", time(4,4), time(5, 1));
   startup_animation("#player1, #player2, #player3, #player4, #player5");
 });
 
